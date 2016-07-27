c97649fab22efb3becff479b71c7085cebc2c1aa
Nikhil Shinde <nikhilshinde57@gmail.com>
2016-07-27 13:05:57 -0400

    Added front end frokm to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,17 @@ socket.on('message',function(message){
 	console.log('New msg!!');
 	console.log(message.text);
 }); 
+
+//Handles submitting of new message
+var $form= jQuery('#message-form');
+
+
+$form.on('submit',function(event){
+	event.preventDefault();
+	var msg=$form.find('input[name=message]');
+	socket.emit('message',{
+		text:msg.val();
+	});
+
+	msg.val('');
+});
